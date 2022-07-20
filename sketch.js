97d4957d72d2a7a83f44a5d0311b55ab3f2c950c
anandkdeol <106873136+anandkdeol@users.noreply.github.com>
2022-07-20 13:00:30 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  createCanvas(400,400);
+Object1=new Student("anand",13,"Year 9")
+Object1.display()
+Object2=new Student("amelia",13,"Year 9")
+Object2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(n, a, y) {
+        this.n = n;
+        this.a = a;
+        this.y = y;
+    }
+display(){
+    console.log(this.n)
+    console.log(this.a)
+    console.log(this.y)
+}}
